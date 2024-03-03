2e028b1ae85814fa91ca5eda466470decc2f89b9
anugrohoreds <agung251295@gmail.com>
2024-03-03 17:53:31 +0700

    second commit

--- cypress/e2e/APITesting/basicAuth.cy.js
+++ cypress/e2e/APITesting/basicAuth.cy.js
@@ -14,4 +14,31 @@ describe("API Testing Using Cypress",()=>{
             expect(response.body.authenticated).to.eq(true)
         })
     });
+
+    const token = "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"
+    it("Github Authentication",()=>{
+        cy.request({
+            method: "GET",
+            url: "https://api.github.com/user/repos",
+            headers: {
+                Authorization: "Bearer "+token
+            }
+        })
+        .then((response)=>{
+            expect(response.status).to.eq(200)
+        })
+    });
+
+    it("API Key Auth",()=>{
+        cy.request({
+            method: "GET",
+            url: "api.openweaathermap.org/data/2.5/forecast/daily?q=Delhi",
+            qs:{
+                appid: "c299a95fc017b92029b3b13046f8a671"
+            }
+        })
+        .then((response)=>{
+            expect(response.status).to.eq(200)
+        })
+    });
 });
